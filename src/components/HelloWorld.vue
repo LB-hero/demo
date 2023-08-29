28809abe18375f5ef8c5d77564668cadfc873652
gaodongxue <1130337942@qq.com>
2023-08-29 23:56:46 +0800

    111

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="picBox">
+    dsad
+	<vue-preview :slides="details" class="、" style="display:inline-block;"></vue-preview>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name:'HelloWorld11111',
+    data() {
+        return {
+           details: [
+            {
+src: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+msrc: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+w: 600,
+h: 400
+}, {
+src: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+msrc: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+w: 600,
+h: 400
+}, {
+src: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+msrc: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+w: 600,
+h: 400
+}, {
+src: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+msrc: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+w: 600,
+h: 400
+},
+            
+           ],
+        };
+    },
+    methods: {
+    //     async getThirdHaikEventList(val) {
+		// 	const params = {
+		// 		v: "v1.0",
+		// 		type: 8008,
+		// 		start: this.pageNumPhoto ? this.pageNumPhoto : '0',
+		// 		limit: '12',
+		// 		deviceIndexCode: val,
+		// 		beginTime: startTime,
+		// 		endTime: endTime,
+		// 	};
+    //         let data = await this.$http.get('/bi/elevatorMonitor/getThirdHaikEventList', { params });
+    //         if (data && Object.keys(data).length) {
+		// 		data.list.forEach(item => {
+		// 			item.w = 600;   //设置以大图浏览时的宽度
+    //                 item.h = 400;     //设置以大图浏览时的高度
+    //                 item.src = item.imageUrl;  //大图
+    //                 item.msrc = item.imageUrl;  //小图
+		// 		})
+		// 		this.details = data.list;
+		// 		this.totalPhoto = data.total;
+    //         }
+		// },
+    },  
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VuePreview from 'vue-preview'
+ 
+Vue.use(VuePreview)
+ Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
